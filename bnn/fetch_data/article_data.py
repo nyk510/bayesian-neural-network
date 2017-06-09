2ba6c6db745261da4ed755f28feae27b49452f05
nyk510 <takamail53@gmail.com>
2017-06-09 16:44:06 +0900

    [Change] 人口データの作成のやり方変更

--- bnn/fetch_data/article_data.py
+++ bnn/fetch_data/article_data.py
@@ -39,8 +39,7 @@ def make_data(size, function_id=1, seed=1):
     :rtype: tuple of (np.array, np.array, function)
     """
     np.random.seed(seed)
-    x = np.sort(np.random.uniform(-1.1, 1.1, size=size)).astype(np.float32)
-    x = x[(x < 0.) | (x > .5)]
+    x = np.sort(np.random.uniform(-1.5, 1.5, size=size)).astype(np.float32).reshape(-1, 1)
     f = None
     if function_id == 1:
         f = func1
